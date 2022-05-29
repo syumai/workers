5c55a2ebf7f7a4149dcd88474c71cd0a1c1b371a
syumai <syumai@gmail.com>
2022-05-29 22:27:46 +0900

    add hello example

---
+++ examples/hello/README.md
@@ -0,0 +1,9 @@
+# hello
+
+* This app just returns a message `Hello, world!`.
+* If url param like `?name=syumai`, then a message `Hello, syumai!` will be returned.
+
+## Demo
+
+* https://hello.syumai.workers.dev/
+* https://hello.syumai.workers.dev/?name=syumai
---
+++ examples/hello/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/syumai/workers"
+)
+
+func main() {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		name := req.URL.Query().Get("name")
+		if name == "" {
+			name = "world"
+		}
+		fmt.Fprintf(w, "Hello, %s!", name)
+	})
+	workers.Serve(handler)
+}

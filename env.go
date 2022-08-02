6a9589752cbda9118a6ff7019e78c385b2a09bf3
syumai <syumai@gmail.com>
2022-08-03 00:00:54 +0900

    fix env docs

--- env.go
+++ env.go
@@ -2,7 +2,7 @@ package workers
 
 // Getenv gets an environment variable set to global object.
 // * https://developers.cloudflare.com/workers/platform/environment-variables/
-// * Technically, this function is just an alias for js.Global().Get(env_name).
+// * Technically, this function is just an alias for js.Global().Get(env_name).String().
 func Getenv(name string) string {
 	return global.Get(name).String()
 }

466065d6d3d3a212a41dd453a58cdaf4fbab9c03
syumai <syumai@gmail.com>
2023-02-26 17:38:38 +0900

    fix last_row_id key name

--- cloudflare/d1/result.go
+++ cloudflare/d1/result.go
@@ -17,7 +17,7 @@ var (
 // LastInsertId returns id of result's last row.
 // If lastRowId can't be retrieved, this method returns error.
 func (r *result) LastInsertId() (int64, error) {
-	v := r.resultObj.Get("meta").Get("lastRowId")
+	v := r.resultObj.Get("meta").Get("last_row_id")
 	if v.IsNull() || v.IsUndefined() {
 		return 0, errors.New("d1: lastRowId cannot be retrieved")
 	}

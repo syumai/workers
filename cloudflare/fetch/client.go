ef831af9d76e324c7d19c40b36d528c098ed1f53
aki-0421 <118268728+aki-0421@users.noreply.github.com>
2023-04-09 09:23:07 +0900

    R: add WithBinding in fetch

--- cloudflare/fetch/client.go
+++ cloudflare/fetch/client.go
@@ -12,9 +12,30 @@ type Client struct {
 	namespace js.Value
 }
 
+// applyOptions applies client options.
+func (c *Client) applyOptions(opts []ClientOption) {
+	for _, opt := range opts {
+		opt(c)
+	}
+}
+
+// ClientOption is a type that represents an optional function.
+type ClientOption func(*Client)
+
+// WithBinding changes the objects that Fetch API belongs to.
+// This is useful for service bindings, mTLS, etc.
+func WithBinding(bind js.Value) ClientOption {
+	return func(c *Client) {
+		c.namespace = bind
+	}
+}
+
 // NewClient returns new Client
-func NewClient() *Client {
-	return &Client{
+func NewClient(opts ...ClientOption) *Client {
+	c := &Client{
 		namespace: jsutil.Global,
 	}
+	c.applyOptions(opts)
+
+	return c
 }

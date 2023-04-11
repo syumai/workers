9e97a4a812816374ec31d96d7a3f951f7f916577
aki-0421 <118268728+aki-0421@users.noreply.github.com>
2023-04-11 21:04:19 +0900

    R: refactor with bind func

---
+++ cloudflare/fetch/bind.go
@@ -0,0 +1,33 @@
+package fetch
+
+import (
+	"errors"
+	"net/http"
+	"syscall/js"
+
+	"github.com/syumai/workers/internal/jshttp"
+	"github.com/syumai/workers/internal/jsutil"
+)
+
+// fetch is a function that reproduces cloudflare fetch.
+// Docs: https://developers.cloudflare.com/workers/runtime-apis/fetch/
+func fetch(namespace js.Value, req *http.Request /* TO DO: options here */) (*http.Response, error) {
+	if namespace.IsUndefined() {
+		return nil, errors.New("fetch function not found")
+	}
+
+	promise := namespace.Call("fetch",
+		// The Request object to fetch.
+		// Docs: https://developers.cloudflare.com/workers/runtime-apis/request
+		jshttp.ToJSRequest(req),
+		// The content of the request.
+		// Docs: https://developers.cloudflare.com/workers/runtime-apis/request#requestinit
+		jsutil.NewObject(),
+	)
+	jsRes, err := jsutil.AwaitPromise(promise)
+	if err != nil {
+		return nil, err
+	}
+
+	return jshttp.ToResponse(jsRes)
+}

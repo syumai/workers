16692e1f71a074ea98f50b76e1c7e2d69a718fb3
zztkm <takumi.basket1682@gmail.com>
2023-04-20 23:52:16 +0900

    add(examples): pages functions example

---
+++ examples/pages-functions/README.md
@@ -0,0 +1,20 @@
+# pages-functions
+
+* This app is the Pages Functions version of hello.
+
+## Development
+
+### Requirements
+
+This project requires these tools to be installed globally.
+
+* wrangler
+* tinygo
+
+### Commands
+
+```
+make dev     # run dev server
+make build   # build Go Wasm binary
+make publish # publish worker
+```
---
+++ examples/pages-functions/functions/hello.js
@@ -0,0 +1,24 @@
+import "../../assets/polyfill_performance.js";
+import "../../assets/wasm_exec.js";
+import mod from "../dist/app.wasm";
+
+const go = new Go();
+
+const readyPromise = new Promise((resolve) => {
+  globalThis.ready = resolve;
+});
+
+const load = WebAssembly.instantiate(mod, go.importObject).then((instance) => {
+  go.run(instance);
+  return instance;
+});
+
+export const onRequest = async (ctx) => {
+  await load;
+  await readyPromise;
+  const {
+    request,
+    env,
+  } = ctx;
+  return handleRequest(request, { env, ctx });
+}

3fed99fa50deae65201d3967d9d6a2e4dd195cb2
aki-0421 <118268728+aki-0421@users.noreply.github.com>
2023-04-26 12:57:36 +0900

    F: add cache API

---
+++ cloudflare/cache/client.go
@@ -0,0 +1,45 @@
+package cache
+
+import (
+	"syscall/js"
+
+	"github.com/syumai/workers/internal/jsutil"
+)
+
+var cache = jsutil.Global.Get("caches")
+
+// Cache
+type Cache struct {
+	// instance - The object that Cache API belongs to.
+	instance js.Value
+}
+
+// applyOptions applies client options.
+func (c *Cache) applyOptions(opts []CacheOption) {
+	for _, opt := range opts {
+		opt(c)
+	}
+}
+
+// CacheOption
+type CacheOption func(*Cache)
+
+// WithNamespace
+func WithNamespace(namespace string) CacheOption {
+	return func(c *Cache) {
+		v, err := jsutil.AwaitPromise(cache.Call("open", namespace))
+		if err != nil {
+			panic("failed to open cache")
+		}
+		c.instance = v
+	}
+}
+
+func New(opts ...CacheOption) *Cache {
+	c := &Cache{
+		instance: cache.Get("default"),
+	}
+	c.applyOptions(opts)
+
+	return c
+}

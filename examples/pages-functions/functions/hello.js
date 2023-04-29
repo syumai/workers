2528bf515cada72bfddc36a403561af5e28fb864
syumai <syumai@gmail.com>
2023-04-30 07:13:30 +0900

    fix example's build commands and paths

--- examples/pages-functions/functions/hello.js
+++ examples/pages-functions/functions/hello.js
@@ -1,6 +1,6 @@
 import "../../assets/polyfill_performance.js";
 import "../../assets/wasm_exec.js";
-import mod from "../dist/app.wasm";
+import mod from "../build/app.wasm";
 
 const go = new Go();
 

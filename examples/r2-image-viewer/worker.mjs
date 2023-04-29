4566ca38522e90387bd677fc8cc5fb709ab5bcd8
syumai <syumai@gmail.com>
2023-04-29 18:30:15 +0900

    update worker.mjs

--- examples/r2-image-viewer/worker.mjs
+++ examples/r2-image-viewer/worker.mjs
@@ -1,22 +1,6 @@
-import "../assets/polyfill_performance.js";
-import "../assets/wasm_exec.js";
+import * as imports from "../assets/shim.mjs";
 import mod from "./dist/app.wasm";
 
-const go = new Go();
+imports.init(mod);
 
-const readyPromise = new Promise((resolve) => {
-  globalThis.ready = resolve;
-});
-
-const load = WebAssembly.instantiate(mod, go.importObject).then((instance) => {
-  go.run(instance);
-  return instance;
-});
-
-export default {
-  async fetch(req, env, ctx) {
-    await load;
-    await readyPromise;
-    return handleRequest(req, { env, ctx });
-  }
-}
+export default { fetch: imports.fetch }

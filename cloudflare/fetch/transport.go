ed546fec093a1e46b6eb0fbad95a4194a9a3bed6
aki-0421 <118268728+aki-0421@users.noreply.github.com>
2023-05-28 13:04:35 +0900

    Add redirect mode

--- cloudflare/fetch/transport.go
+++ cloudflare/fetch/transport.go
@@ -9,9 +9,12 @@ import (
 type transport struct {
 	// namespace - Objects that Fetch API belongs to. Default is Global
 	namespace js.Value
+	redirect  RedirectMode
 }
 
 // RoundTrip replaces http.DefaultTransport.RoundTrip to use cloudflare fetch
 func (t *transport) RoundTrip(req *http.Request) (*http.Response, error) {
-	return fetch(t.namespace, req)
+	return fetch(t.namespace, req, &RequestInit{
+		Redirect: t.redirect,
+	})
 }

565d8fa322c7ae2b6ad4601d8ebb877412a879c5
Jose Juan Montiel <josejuan.montiel@gmail.com>
2025-04-21 18:03:12 +0200

    remane

--- cloudflare/ai/call.go
+++ cloudflare/ai/call.go
@@ -7,19 +7,19 @@ import (
 	"io"
 )
 
-type AIMock struct {
+type AI struct {
 	instance string
 }
 
-func NewNamespace(varName string) (*AIMock, error) {
+func NewNamespace(varName string) (*AI, error) {
 	fmt.Println("NewNamespace called with varName:", varName)
 	if varName == "" {
 		return nil, fmt.Errorf("%s is undefined", varName)
 	}
-	return &AIMock{instance: varName}, nil
+	return &AI{instance: varName}, nil
 }
 
-func (ns *AIMock) WaitUntil(task func()) {
+func (ns *AI) WaitUntil(task func()) {
 	fmt.Println("WaitUntil called")
 	go func() {
 		task()
@@ -27,12 +27,12 @@ func (ns *AIMock) WaitUntil(task func()) {
 	fmt.Println("Task completed")
 }
 
-func (ns *AIMock) Run(key string, opts map[string]interface{}) (string, error) {
+func (ns *AI) Run(key string, opts map[string]interface{}) (string, error) {
 	fmt.Println("Run called with key:", key, "and opts:", opts)
 	return "mocked response", nil
 }
 
-func (ns *AIMock) RunReader(key string, opts map[string]interface{}) (io.Reader, error) {
+func (ns *AI) RunReader(key string, opts map[string]interface{}) (io.Reader, error) {
 	fmt.Println("RunReader called with key:", key, "and opts:", opts)
 	return io.NopCloser(nil), nil
 }

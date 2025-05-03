be67de1b6321bd3621ab7575fad769f94720baef
Jose Juan Montiel <josejuan.montiel@gmail.com>
2025-05-03 10:05:46 +0200

    remove log waiting to global aproach.

--- cloudflare/ai/call.go
+++ cloudflare/ai/call.go
@@ -12,7 +12,6 @@ type AI struct {
 }
 
 func New(varName string) (*AI, error) {
-	fmt.Println("NewNamespace called with varName:", varName)
 	if varName == "" {
 		return nil, fmt.Errorf("%s is undefined", varName)
 	}
@@ -20,16 +19,13 @@ func New(varName string) (*AI, error) {
 }
 
 func (ns *AI) Run(key string, opts map[string]any) (string, error) {
-	fmt.Println("Run called with key:", key, "and opts:", opts)
 	return "mocked response", nil
 }
 
 func (ns *AI) RunWithOutJson(key string, opts map[string]any) (string, error) {
-	fmt.Println("Run called with key:", key, "and opts:", opts)
 	return "mocked response", nil
 }
 
 func (ns *AI) RunReader(key string, opts map[string]any) (io.Reader, error) {
-	fmt.Println("RunReader called with key:", key, "and opts:", opts)
 	return io.NopCloser(nil), nil
 }
